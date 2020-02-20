97bcbec141413aff5f994e94d35ce66fb086e713
Finn Wilkinson <finnwilkinson5@gmail.com>
2020-02-20 19:06:54 +0000

    Finished CW1

--- coursework1/seven_segments_p3.py
+++ coursework1/seven_segments_p3.py
@@ -2,8 +2,9 @@
 #so submission.README
 
 from math import *
+import numpy as np
 
-#from submission import *
+from submission import *
 
 def seven_segment(pattern):
 
@@ -11,14 +12,14 @@ def seven_segment(pattern):
         if a==1:
             return True
         return False
-    
+
 
     def hor(d):
         if d:
             print(" _ ")
         else:
             print("   ")
-    
+
     def vert(d1,d2,d3):
         word=""
 
@@ -26,20 +27,20 @@ def seven_segment(pattern):
             word="|"
         else:
             word=" "
-        
+
         if d3:
             word+="_"
         else:
             word+=" "
-        
+
         if d2:
             word+="|"
         else:
             word+=" "
-        
+
         print(word)
 
-    
+
 
     pattern_b=list(map(to_bool,pattern))
 
@@ -52,9 +53,9 @@ def seven_segment(pattern):
         if pattern_b[7+i]:
             number+=pow(2,i)
     print(int(number))
-        
-#submission=Submission("your_name")
-#submission.header("Your Name")
+
+submission=Submission("Finn_Wilkinson")
+submission.header("Finn Wilkinson")
 
 six=[1,1,-1,1,1,1,1,-1,1,1,-1]
 three=[1,-1,1,1,-1,1,1,1,1,-1,-1]
@@ -64,18 +65,56 @@ seven_segment(three)
 seven_segment(six)
 seven_segment(one)
 
+weight_matrix = np.asarray([[0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+                            [0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+                            [0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+                            [0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+                            [0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+                            [0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+                            [0.0,0.0,0.0,0.0,0.0,0.0,0.0]])
+
+for i in range(0,7) :
+    for j in range(0,7) :
+        if (i != j) :
+            weight_matrix[i,j] = (1/3) * ((six[i]*six[j]) + (three[i]*three[j]) + one[i]*one[j])
+
+print(weight_matrix)
+
 ##this assumes you have called your weight matrix "weight_matrix"
-#submission.section("Weight matrix")
-#submission.matrix_print("W",weight_matrix)
+submission.section("Weight matrix")
+submission.matrix_print("W",weight_matrix)
+
+def McCulloch_Pitts_Formula(weight_matrix, test) :
+    next = [0.0,0.0,0.0,0.0,0.0,0.0,0.0, test[7], test[8], test[9], test[10]]
+    while (1) :
+        for i in range(0,7) :
+            for j in range(0,7) :
+                next[i] = next[i] + (weight_matrix[i,j] * test[j] )
+
+            if (next[i] > 0.0) :
+                next[i] = 1
+
+            else :
+                next[i] = -1
+
+        if (next == test) :
+            seven_segment(test)
+            return next
+
+        else :
+            test = next
+
+        seven_segment(test)
+
 
 print("test1")
-#submission.section("Test 1")
+submission.section("Test 1")
 
 test=[1,-1,1,1,-1,1,1,-1,-1,-1,-1]
 
-
 seven_segment(test)
-#submission.seven_segment(test)
+test = McCulloch_Pitts_Formula(weight_matrix, test)
+submission.seven_segment(test)
 ##for COMSM0027
 
 ##where energy is the energy of test
@@ -90,9 +129,10 @@ seven_segment(test)
 print("test2")
 
 test=[1,1,1,1,1,1,1,-1,-1,-1,-1]
-#submission.section("Test 1")
+submission.section("Test 1")
 
 seven_segment(test)
+test = McCulloch_Pitts_Formula(weight_matrix, test)
 
 
 #submission.seven_segment(test)
@@ -108,7 +148,4 @@ seven_segment(test)
 #for the final submission it should also output to submission on each step
 
 
-#submission.bottomer()
-
-
-
+submission.bottomer()
